25b4aa8a11dfff9a36a8485208a79813ef5040d1
Balázs Tolvaj <tolvibalazs@gmail.com>
2024-03-17 18:43:12 +0100

    Connect to Jitsi Api Client html elements added.

--- JitsiApiClient/src/app/jitsi-api-client/jitsi-api-client.component.ts
+++ JitsiApiClient/src/app/jitsi-api-client/jitsi-api-client.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+
+declare var JitsiMeetExternalAPI: any;
 
 @Component({
   selector: 'app-jitsi-api-client',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './jitsi-api-client.component.css'
 })
 export class JitsiApiClientComponent {
+  @Input() meetingRoom!: string;
+  @Input() name!: string;
 
 }
