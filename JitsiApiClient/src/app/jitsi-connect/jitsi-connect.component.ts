d3715ad5e6983eae08bc7db17211e49a9ecf2ccd
Balázs Tolvaj <tolvibalazs@gmail.com>
2024-03-17 18:14:49 +0100

    Jitsi lov level api added

--- JitsiApiClient/src/app/jitsi-connect/jitsi-connect.component.ts
+++ JitsiApiClient/src/app/jitsi-connect/jitsi-connect.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+declare var JitsiMeetJS: any;
 
 @Component({
   selector: 'app-jitsi-connect',
   templateUrl: './jitsi-connect.component.html',
   styleUrl: './jitsi-connect.component.css'
 })
-export class JitsiConnectComponent {
+export class JitsiConnectComponent implements OnInit{
 
+  ngOnInit(): void {
+    JitsiMeetJS.init();
+
+    const connection = new JitsiMeetJS.JitsiConnection(null, null, {hosts: {domain: 'meet.jit.si'}});
+    connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_ESTABLISHED, () => {
+      console.log(connection);
+    });
+    connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_FAILED, () => {
+      console.log('The connection failed.');
+    });
+    connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_DISCONNECTED, () => {
+      console.log("Connection disconnected");
+    });
+
+    connection.connect();
+  }
 }
