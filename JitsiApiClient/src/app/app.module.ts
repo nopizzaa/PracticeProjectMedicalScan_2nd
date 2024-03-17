d656778847f11244931f2e604c58906fdffbac5f
Balázs Tolvaj <tolvibalazs@gmail.com>
2024-03-17 17:45:07 +0100

    JitsiConnectComponent angular component added to project.

--- JitsiApiClient/src/app/app.module.ts
+++ JitsiApiClient/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JitsiConnectComponent } from './jitsi-connect/jitsi-connect.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JitsiConnectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component: JitsiConnectComponent, pathMatch: 'full'},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
