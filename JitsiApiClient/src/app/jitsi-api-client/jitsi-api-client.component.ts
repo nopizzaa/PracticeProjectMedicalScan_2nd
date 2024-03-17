bcfffbfaef9c909b1282ab7c40504fa8c7aba47c
Balázs Tolvaj <tolvibalazs@gmail.com>
2024-03-17 18:00:35 +0100

    JitsiConnectComponent layout finalized and JitsiApiClientComponent added.

---
+++ JitsiApiClient/src/app/jitsi-api-client/jitsi-api-client.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jitsi-api-client',
+  templateUrl: './jitsi-api-client.component.html',
+  styleUrl: './jitsi-api-client.component.css'
+})
+export class JitsiApiClientComponent {
+
+}
